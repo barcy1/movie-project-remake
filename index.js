fa2e3338fc69fecbbdf2f5af0990aee7287836c2
Bar Yaish <baryaish21@gmail.com>
2022-12-19 14:23:34 +0200

    only need to change the index rendering for movies that already in the watchlist!

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const wrapper= document.querySelector('.wrapper');
-const searchBtn=document.querySelector('#search-btn');
-const searchInput=document.querySelector('.search-box');
 const apiUrl=`http://www.omdbapi.com/?i=tt3896198&apikey=39fdc95`;
-const check=document.querySelector('.check');
+
 const main=document.body;
 let moviesArray=[];
 let watchListArray=[];
+let isSearched=false;
+
+
 
 document.addEventListener('click',(event)=>{
+    
+    const searchInput=document.querySelector('.search-box');
     //setting the event handler for the search button click
     if(event.target.id==='search-btn'){
         console.log('clicked');
@@ -18,53 +20,54 @@ document.addEventListener('click',(event)=>{
     
 
      if(event.target.dataset.icon){
-
-        watchListArray= JSON.parse(localStorage.getItem('watchList') || '[]');
+        
+        let pressedIcon=event.target.dataset.icon;
         let pressedId=event.target.dataset.id;
-        //console.log(pressedId);
-        const iconChange=document.querySelectorAll('.add-remove >:first-child');
-        //console.log(moviesArray);
-        let movieId=moviesArray.find(movie=> movie.imdbID===pressedId);
-       // console.log(movieId); 
-        //========handeling ADDING movies to watchlist and changing the btn and text.======
-        if(event.target.dataset.icon==='plus'){       
-            for(let i=0; i<iconChange.length;i++){
-                    if(iconChange[i].dataset.id===pressedId){
-                        iconChange[i].innerHTML=`${watchListBtn(movieId,false)}`;
-                        break;                                           
-                    }            
-                }
-            //adding movie to watchlist and save the array ont he localStorage. 
-            
-            watchListArray.push(movieId);
-            localStorage.setItem('watchList',JSON.stringify(watchListArray));               
-            console.log(watchListArray);
-            
-            
-            //console.log(localCheck);
-        }else{
-            for(let i=0; i<iconChange.length;i++){
-                if(iconChange[i].dataset.id===pressedId){
-                    iconChange[i].innerHTML=`${watchListBtn(movieId,true)}`;                   
-                    break;                
-                }                            
-            }
-            if(watchListArray.length>0){
-                for(let i=0; i<watchListArray.length;i++){
-                    if(watchListArray[i].imdbID===movieId.imdbID){
-                        watchListArray.splice(i,1);
-                        break;
+        const iconChange=document.querySelectorAll('.add-remove');
+        var movieId=moviesArray.find(movie=> movie.imdbID===pressedId);
+        watchListArray= JSON.parse(localStorage.getItem('watchList') || '[]');
+        
+           
+           
+            //========handeling ADDING movies to watchlist and changing the btn and text.======
+        if(pressedIcon==='plus'){ 
+            console.log(movieId);      
+                for(let i=0; i<iconChange.length;i++){
+                        if(iconChange[i].dataset.id===pressedId){
+                            iconChange[i].innerHTML=`${watchListBtn(movieId,false)}`;
+                            break;                                           
+                        }            
                     }
-                }
+                //adding movie to watchlist and save the array ont he localStorage. 
                 
-                localStorage.setItem('watchList',JSON.stringify(watchListArray));
-                watchListArray= JSON.parse(localStorage.getItem('watchList') || '[]');
+                watchListArray.push(movieId);
+                localStorage.setItem('watchList',JSON.stringify(watchListArray));               
                 console.log(watchListArray);
-            } 
-
-        }      
+        }
+        if(pressedIcon==='minus'){            
+            console.log(pressedIcon);
+            console.log(pressedId);
+            console.log(iconChange);
+            var movieId=watchListArray.find(movie=> movie.imdbID===pressedId);
+            console.log(movieId);
+            if(watchListArray.length>0){
+              let movieToDelete=watchListArray.find(movie=>movie.imdbID===movieId.imdbID);
+              let index=watchListArray.indexOf(movieToDelete);
+              watchListArray.splice(index,1);
+              localStorage.setItem('watchList',JSON.stringify(watchListArray));
+              watchListArray= JSON.parse(localStorage.getItem('watchList') || '[]');
+              renderMovieCard(watchListArray,false);
+              if(watchListArray.length===0){
+                watchlistRender();
+              }
+            }
+           
+             
+        }
        
-    } 
+     }         
+       
+     
     
 })
 
@@ -95,7 +98,7 @@ const gettingHtml= (movie,plusOrMinus)=>{
                             <div class="movie-details">
                                 <span class="movie-length">${movie.Runtime}</span>
                                 <span class="movie-type">${movie.Genre}</span>
-                                <div class="add-remove"> 
+                                <div class="add-remove" data-id=${movie.imdbID}> 
                                     ${watchListBtn(movie,plusOrMinus)}                  
                                     
                                 </div>
@@ -112,10 +115,11 @@ const gettingHtml= (movie,plusOrMinus)=>{
     )
 }
 
-const renderMovieCard = (moviesArray,plusOrMinus)=>{
+const renderMovieCard = (movies,plusOrMinus)=>{
+    const check=document.querySelector('.check');
     console.log(plusOrMinus);
-    console.log(moviesArray);
-    check.innerHTML=`<section>${moviesArray.map(movie=>(gettingHtml(movie,plusOrMinus))).join('')}</setion>`
+    console.log(movies);
+    check.innerHTML=`<section>${movies.map(movie=>(gettingHtml(movie,plusOrMinus))).join('')}</setion>`
     /* check.innerHTML =`<section>${(moviesArray.map(movie=>{gettingHtml(movie, plusOrMinus)})).join('')}</section>` */
 }
 
@@ -139,7 +143,7 @@ const TitleSearch= async (searchValue)=>{
 
 
 
-/* const indexRender=()=>{
+const indexRender=()=>{
     main.innerHTML=`
         <div class="wrapper">
         <div class="header">        
@@ -150,20 +154,68 @@ const TitleSearch= async (searchValue)=>{
             <input type="text" class="search-box">
             <button id='search-btn' type="submit">search</button>
         </div>
-        <div class="check">
+        <div class="check"> 
+            <div class="indexplace">
+                <img src='images/indexplaceholder-film.png'>
+                <span>Search for a movie!</span>                
+            </div>
+        </div>    
+    `;
+} 
 
 
-        </div>
+
+const watchlistRender=()=>{
+    main.innerHTML=`
+        <div class="wrapper">
+            <div class="header">        
+                <h1>WathList</h1>
+                <a href="index.html">Search for movies!</a>        
+           </div>
+          <div class="check">
+            <div class='watchlist-container'>
+                <a href='index.html'><span>Go to search page to find some movies!</span></a>
+            </div>
+        </div> 
+        
     
     `;
+}
+
+
+
+
+window.addEventListener('load',()=>{
+    watchListArray= JSON.parse(localStorage.getItem('watchList') || '[]')
+    if(main.id==='index'){
+        indexRender(); 
+        
+    }else{
+        if(watchListArray.length>0){
+            watchlistRender();
+            renderMovieCard(watchListArray,false);
+        }
+        else{
+            watchlistRender();
+        }
+    }    
+  
+
+})
+
+
+
+ 
+/* const watchlistPlaceholder=()=>{
+    watch
 } */
 
 
 
 
-/* else{
+/*  else{
     renderMovieCard(watchListArray,plusOrMinus);
-} */
+}  */
 
 
 
@@ -206,4 +258,11 @@ const TitleSearch= async (searchValue)=>{
 </div>
         
 </div>
-<hr> */
+<hr> */ 
+
+
+
+
+
+
+
